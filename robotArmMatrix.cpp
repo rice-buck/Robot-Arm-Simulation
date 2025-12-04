f37556a57e91144bf88f14da79e6c78e1d531cc1
rice-buck <rhysobuckeye@outlook.com>
2025-12-03 22:11:58 -0800

    changed arm colors and added a claw

--- robotArmMatrix.cpp
+++ robotArmMatrix.cpp
@@ -14,7 +14,7 @@ int main(){
     sf::ContextSettings settings;
     settings.antiAliasingLevel = 4;
 
-    sf::RenderWindow window(sf::VideoMode({800, 600}), ":)", sf::State::Windowed, settings);
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Arm Simulation", sf::State::Windowed, settings);
 
     //view for arm coordinates 
     sf::View worldView;
@@ -28,31 +28,60 @@ int main(){
 
     //three dots
     sf::CircleShape d0;
-    d0.setFillColor(sf::Color(0,170, 255));     
+    d0.setFillColor(sf::Color(0, 0, 0));
+    d0.setOutlineColor(sf::Color(255, 165, 0));  
+    d0.setOutlineThickness(-5.f);  
     sf::CircleShape d1;
-    d1.setFillColor(sf::Color(0,170, 255));
+    d1.setFillColor(sf::Color(0, 0, 0));
+    d1.setOutlineColor(sf::Color(255, 165, 0)); 
+    d1.setOutlineThickness(-5.f); 
     sf::CircleShape d2;
-    d2.setFillColor(sf::Color(0,170, 255));
+    d2.setFillColor(sf::Color(0, 0, 0));
+    d2.setOutlineColor(sf::Color(255, 165, 0)); 
+    d2.setOutlineThickness(-5.f); 
     sf::CircleShape d3;
-    d3.setFillColor(sf::Color(0,170, 255));
+    d3.setFillColor(sf::Color(0, 0, 0));
+    d3.setOutlineColor(sf::Color(255, 165, 0)); 
+    d3.setOutlineThickness(-5.f); 
 
+    //plot point
+    sf::CircleShape point;
+    point.setFillColor(sf::Color(255, 0, 0));
+
+    //floor line
+    sf::RectangleShape floor;
+    floor.setFillColor(sf::Color(0, 0, 0));
 
     //create lines
     sf::RectangleShape line1;
-    line1.setFillColor(sf::Color(0, 170, 255));
+    line1.setFillColor(sf::Color(255, 255, 0));
     sf::RectangleShape line2;
-    line2.setFillColor(sf::Color(0, 170, 255));
+    line2.setFillColor(sf::Color(255, 255, 0));
     sf::RectangleShape line3;
-    line3.setFillColor(sf::Color(0, 170, 255));
+    line3.setFillColor(sf::Color(255, 255, 0));
+
+    //lines for claw (T= top, B= bottom)
+    sf::RectangleShape clawT1;
+    clawT1.setFillColor(sf::Color(255, 165, 0));
+    sf::RectangleShape clawT2;
+    clawT2.setFillColor(sf::Color(255, 165, 0));
+
+
+    sf::RectangleShape clawB1;
+    clawB1.setFillColor(sf::Color(255, 165, 0));
+    sf::RectangleShape clawB2;
+    clawB2.setFillColor(sf::Color(255, 165, 0));
+    
+
 
     //lengths and angles
     float L1 = 100;
     float L2 = 75;
     float L3 = 50;
 
-    float theta1 = 30 * (M_PI / 180.0);
-    float theta2 = -20 * (M_PI / 180.0);
-    float theta3 = 45 * (M_PI / 180.0);
+    float theta1 = -12.4 * (M_PI / 180.0);
+    float theta2 = 85.12 * (M_PI / 180.0);
+    float theta3 = 17.29 * (M_PI / 180.0);
 
     //load font for text
     sf::Font font("/Users/rhysbuckeye/Library/Fonts/DS-DIGIT.TTF");
@@ -69,26 +98,26 @@ int main(){
 
 }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q)){
-        theta1 += 0.0005f;
+        theta1 += 0.0001f;
         std::cout << "Theta 1: " << theta1 * (180.0 / M_PI) << "\n";
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)){
-        theta1 -= 0.0005f;
+        theta1 -= 0.0001f;
         std::cout << "Theta 1: " << theta1 * (180.0 / M_PI) << "\n";
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)){
-        theta2 += 0.0005f;
+        theta2 += 0.0001f;
         std::cout << "Theta 2: " << theta2 * (180.0 / M_PI) << "\n";
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)){
-        theta2 -= 0.0005f;
+        theta2 -= 0.0001f;
         std::cout << "Theta 2: " << theta2 * (180.0 / M_PI) << "\n";
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z)){
-        theta3 += 0.0005f;
+        theta3 += 0.0001f;
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::X)){
-        theta3 -= 0.0005f;
+        theta3 -= 0.0001f;
     }
 
     //create text for joint angles display
@@ -102,14 +131,14 @@ int main(){
     sf::Text jointAngleTwoTxt(font, "", 20);
     jointAngleTwoTxt.setPosition({-400.f, -250.f});
     std::ostringstream ss2;
-    ss2 << std::fixed << std::setprecision(2) << ((theta1 + theta2) * 180.0 / M_PI);
+    ss2 << std::fixed << std::setprecision(2) << ((theta2) * 180.0 / M_PI);
     jointAngleTwoTxt.setString("Joint angle 2: " + ss2.str());
 
 
     sf::Text jointAngleThreeTxt(font, "", 20);
     jointAngleThreeTxt.setPosition({-400.f, -200.f});
     std::ostringstream ss3;
-    ss3 << std::fixed << std::setprecision(2) << ((theta1 + theta2 + theta3) * 180.0 / M_PI);
+    ss3 << std::fixed << std::setprecision(2) << ((theta3) * 180.0 / M_PI);
     jointAngleThreeTxt.setString("Joint angle 3: " + ss3.str());
 
 
@@ -140,15 +169,38 @@ int main(){
         d3.setRadius(radius);
         d3.setOrigin({radius, radius});
 
-        //set line size and origin 
-        line1.setSize({100.f, width});
+        //point
+        point.setRadius(radius);
+        point.setOrigin({radius, radius});
+
+        //set floor origin 
+        floor.setOrigin({75.f, 2.5f});
+
+        //set line origin and outline 
+        line1.setOutlineColor(sf::Color(255, 165, 0));
+        line1.setOutlineThickness(5.f);
         line1.setOrigin({0.f, width/2});
 
-        line2.setSize({100.f, width});
+        line2.setOutlineColor(sf::Color(255, 165, 0));
+        line2.setOutlineThickness(5.f);
         line2.setOrigin({0.f, width/2});
 
-        line3.setSize({100.f, width});
+        line3.setOutlineColor(sf::Color(255, 165, 0));
+        line3.setOutlineThickness(5.f);
         line3.setOrigin({0.f, width/2});
+
+        //claw 
+        clawT1.setSize({25.f, width});
+        clawT1.setOrigin({0.f, width/2});
+
+        clawT2.setSize({25.f, width});
+        clawT2.setOrigin({0.f, width/2});
+        
+        clawB1.setSize({25.f, width});
+        clawB1.setOrigin({0.f, width/2});
+
+        clawB2.setSize({25.f, width});
+        clawB2.setOrigin({0.f, width/2});
 
         // Base point
         Vector3 p0(0, 0, 1);
@@ -200,23 +252,28 @@ int main(){
     //window with inverted y for plotting
     window.setView(worldView);
 
-            //draw dots
+            //dot values
             float p0x = p0.data[0];
             float p0y = p0.data[1];
             d0.setPosition({p0x, p0y});
-            window.draw(d0);
+            
             float p1x = p1.data[0];
             float p1y = p1.data[1]; 
             d1.setPosition({p1x, p1y});
-            window.draw(d1);
+            
             float p2x = p2.data[0];
             float p2y = p2.data[1];
             d2.setPosition({p2x, p2y});
-            window.draw(d2);
+            
             float p3x = p3.data[0];
             float p3y = p3.data[1];
             d3.setPosition({p3x, p3y});
-            window.draw(d3);
+
+            
+            //draw floor
+            floor.setSize({150.f, 3.f});
+            floor.setPosition({p0x, p0y - 9.f});
+            window.draw(floor);
 
             //draw lines
             line1.setSize({L1, width});
@@ -233,6 +290,51 @@ int main(){
             line3.setRotation(sf::radians(theta1 + theta2 + theta3));
             line3.setPosition({p2x, p2y});
             window.draw(line3);
+
+            clawT1.setRotation(sf::radians(theta1 + theta2 + theta3 + 0.7854f));
+            clawT1.setPosition({p3x, p3y});
+            window.draw(clawT1);
+
+            //find end of first T1 claw rectangle
+            sf::Vector2f topRightT1Local(
+            clawT1.getSize().x ,  // full width from origin
+            -clawT1.getSize().y + 11.f  // up half height from origin
+            );
+
+            sf::Vector2f topRightT1World = clawT1.getTransform().transformPoint(topRightT1Local);
+
+            clawT2.setRotation(sf::radians(theta1 + theta2 + theta3 - 0.523599f));
+            clawT2.setPosition(topRightT1World);
+            window.draw(clawT2);
+
+            clawB1.setRotation(sf::radians(theta1 + theta2 + theta3 - 0.7854f));
+            clawB1.setPosition({p3x, p3y});
+            window.draw(clawB1);
+
+            //find end of first B1 claw rectangle
+            sf::Vector2f topRightB1Local(
+            clawB1.getSize().x ,  // full width from origin
+            -clawB1.getSize().y + 7.f  // up half height from origin
+            );
+
+            sf::Vector2f topRightB1World = clawB1.getTransform().transformPoint(topRightB1Local);
+
+            clawB2.setRotation(sf::radians(theta1 + theta2 + theta3 + 0.523599f));
+            clawB2.setPosition(topRightB1World);
+            window.draw(clawB2);
+
+            //draw dots for joints 
+            //draw after line so circles render over lines
+            window.draw(d0);
+            window.draw(d1);
+            window.draw(d2);
+            window.draw(d3);
+
+            //draw point
+            float pointX = 120.f;
+            float pointY = 100.f;
+            point.setPosition({pointX, pointY});
+            window.draw(point);
 
         
         //sets y back to normal for text
